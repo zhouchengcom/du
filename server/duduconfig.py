82cf451868125db89fee006da8eee2cc0668e7fa
zhou cheng <zhoucheng@kingsoft.com>
2018-08-15 11:30:03 +0800

    adjust config

--- server/duduconfig.py
+++ server/duduconfig.py
@@ -1,19 +1,19 @@
 
 
 MINIO = {
-    "host": "10.11.65.34:32769",
+    "host": "minio:9123",
     "akey": "M4UE4EE0UV36DFSMBAC2",
     "skey": "Vu8+9SWWjn/6Hlc+AdxjZv64wCj8kMQKI5nBNNcK",
     "secure": False,
 }
 
-REDIS = "redis://10.11.65.34:6379/0"
+REDIS = "redis://redis:6379/0"
 
 
 SCAN_TYPE = ["WindowsDefender"]
 
 
-BROKER_URL = 'redis://10.11.65.34:6379/3'
-CELERY_RESULT_BACKEND = 'redis://10.11.65.34:6379/4'
+BROKER_URL = 'redis://redis:6379/3'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/4'
 
 HOST ="10.11.65.34"
